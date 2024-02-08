27888ca57e65cdf8de27b8ff8f775477ecf1a12e
Naomi van den Ameele @Library <n.m.vandenameele@tudelft.nl>
2024-02-08 14:56:36 +0100

    count_lines.py: added some lines in the docsstring
    more text here that explains why i did this..

--- count_lines.py
+++ count_lines.py
@@ -1,7 +1,15 @@
 import sys
 
+"""
+Aim: this script counts the numbers of lines in standeard input
++Input: string form the command line
+
+"""
+
 count = 0 
 
 for line in sys.stdin:
 	count += 1 
+
+ print(count, "lines in  standard input")
 
